15062d521084b2261b73a286fe640ed7da534967
leorvergani <vergani@outlook.com.br>
2024-08-13 22:42:02 -0300

    update

--- cadastroPet.js
+++ cadastroPet.js
@@ -1,16 +1,25 @@
 function adicionarPet(event) {
+    console.log("ENTRANDO na funcao")
     event.preventDefault() // evita da tela recarregar :)
 
-    const foto = document.getElementById("foto").value
-    const nome = document.getElementById("nome").value
-    const idade = document.getElementById("idade").value
-    const cor = document.getElementById("cor").value
-    const descricao = document.getElementById("descricao").value
+    const foto = document.getElementById('foto').value
+    const nome = document.getElementById('nome').value 
+    const idade = document.getElementById('idade').value 
+    const cor = document.getElementById('cor').value
+    const descricao = document.getElementById('descricao').value
+    const tipo = document.getElementById('tipo').value
 
+
+    if(nome === "") {
+       // alert("Nome é obrigatório")
+        document.getElementById('nome').style.borderColor = "red"
+        document.getElementById('nome').style.borderWidth = "2px"
+        document.getElementById('error-nome').innerText = "Nome é obrigatório"
+    }
+    
+    /* 2 - colocar data */  
 }
 
-
-
-document // Seu documento HTML
-.getElementById("form-pet") // ir no documento e localizar o elemento com o id form-get
-.addEventListener("submit", adicionarPet) // adicionar um evento de submissão vinculado a função adicionarPet
+document // seu documento HTML
+.getElementById('form-pet') // ir no documento e localizar o elemento com id form-pet
+.addEventListener('submit', adicionarPet) // adicionar um evento de submissão vinculado a funcao adicionar pet
