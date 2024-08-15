5b5fdfdcee1bb3eb926de7c79ca3f888141eabd9
leorvergani <vergani@outlook.com.br>
2024-08-14 22:34:52 -0300

    update

--- cadastroPet.js
+++ cadastroPet.js
@@ -1,5 +1,4 @@
 function adicionarPet(event) {
-    console.log("ENTRANDO na funcao")
     event.preventDefault() // evita da tela recarregar :)
 
     const foto = document.getElementById('foto').value
@@ -9,13 +8,77 @@ function adicionarPet(event) {
     const descricao = document.getElementById('descricao').value
     const tipo = document.getElementById('tipo').value
 
+    /*===== Validação do formulário ===== */
+    if(!foto){
+        document.getElementById('foto').style.borderColor = 'red'
+        document.getElementById('foto').style.borderWidth = '2px'
+        document.getElementById('error-foto').innerText = 'Foto é obrigatório *'
+    } else {
+        document.getElementById('foto').style.borderColor = ''
+        document.getElementById('foto').style.borderWidth = ''
+        document.getElementById('error-foto').innerText = ''
+    }
+
 
     if(nome === "") {
        // alert("Nome é obrigatório")
         document.getElementById('nome').style.borderColor = "red"
         document.getElementById('nome').style.borderWidth = "2px"
-        document.getElementById('error-nome').innerText = "Nome é obrigatório"
+        document.getElementById('error-nome').innerText = "Nome é obrigatório *"
+    } else {
+        document.getElementById('nome').style.borderColor = ""
+        document.getElementById('nome').style.borderWidth = ""
+        document.getElementById('error-nome').innerText = ""
     }
+
+    if(cor === "#802600") {
+        document.getElementById('cor').style.borderColor = "red"
+        document.getElementById('cor').style.borderWidth = "2px"
+        document.getElementById('error-cor').innerText = "Cor é obrigatório *"
+    } else {
+        document.getElementById('cor').style.borderColor = ""
+        document.getElementById('cor').style.borderWidth = ""
+        document.getElementById('error-cor').innerText = ""
+    }
+
+    if(!descricao) {
+        document.getElementById('descricao').style.borderColor = "red"
+        document.getElementById('descricao').style.borderWidth = "2px"
+        document.getElementById('error-descricao').innerText = "Descrição é obrigatório *"
+    } else {
+        document.getElementById('descricao').style.borderColor = ""
+        document.getElementById('descricao').style.borderWidth = ""
+        document.getElementById('error-descricao').innerText = ""
+    }
+
+    if(!tipo) {
+        document.getElementById('tipo').style.borderColor = "red"
+        document.getElementById('tipo').style.borderWidth = "2px"
+        document.getElementById('error-tipo').innerText = "Tipo é obrigatório *"
+    } else {
+        document.getElementById('tipo').style.borderColor = ""
+        document.getElementById('tipo').style.borderWidth = ""
+        document.getElementById('error-tipo').innerText = ""
+    }
+
+
+    const pet = {
+        id: crypto.randomUUID,
+        foto: foto,
+        nome: nome,
+        idade: idade,
+        cor: cor,
+        descricao: descricao,
+        tipo: tipo
+    }
+
+    let listaNoLocalStorage = JSON.parse(localStorage.getItem("pets"))    // vai no local storage e pega a lista
+    
+    if(listaNoLocalStorage === null) listaNoLocalStorage = []
+
+    listaNoLocalStorage.push(pet) 
+
+    localStorage.setItem("pets", JSON.stringify(listaNoLocalStorage)) // salvar no local storage
     
     /* 2 - colocar data */  
 }
